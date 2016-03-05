2f20fda9ad0bbe3648844acae7553cc310daf4ef
kushalchawla <kushalchawla18@gmail.com>
2016-03-05 15:38:16 +0530

    shared data variables made static

--- project2/part1/shared_data.h
+++ project2/part1/shared_data.h
@@ -1,6 +1,6 @@
 #include <vector>
 #include <string>
-string RE;
-std::vector< vector< vector<int> > > NFA;
-std::vector< vector<int> > DFA;	
-std::vector< vector<int> > min_DFA;
+static string RE;
+static std::vector< vector< vector<int> > > NFA;
+static std::vector< vector<int> > DFA;	
+static std::vector< vector<int> > min_DFA;
