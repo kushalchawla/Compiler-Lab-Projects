3f7405c318eb155120b45b592fc747861e088913
kushalchawla <kushalchawla18@gmail.com>
2016-03-05 16:51:53 +0530

    start state added in shared data

--- project2/part1/regex_to_nfa.cpp
+++ project2/part1/regex_to_nfa.cpp
@@ -1,13 +1,16 @@
 //kushal - step 1
+//each nfa is a pair of vector< vector< vector<int> >  > and int start state, int(final state)
+
 #include <iostream>
 #include "regex_to_nfa.h"
 #include "shared_data.h"
 
 using namespace std;
 
+
 void re_to_nfa()
 {
 	cout<<"Enter the regular expression !";
 	cin>>RE;
-	
+	R();
 }
--- project2/part1/shared_data.h
+++ project2/part1/shared_data.h
@@ -6,12 +6,15 @@ static string RE;
 
 //after step 1 
 static std::vector< vector< vector<int> > > NFA;
+static int nfa_start_state;
 static std::set<int> nfa_final_states;
 
 //after step 2
-static std::vector< vector<int> > DFA;	
+static std::vector< vector<int> > DFA;
+static int dfa_start_state;	
 static std::set<int> dfa_final_states;
 
 //Final Output
 static std::vector< vector<int> > min_DFA;
+static int mindfa_start_state;
 static std::set<int> mindfa_final_states;
