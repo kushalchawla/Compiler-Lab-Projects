500994522346d9650b13309dedb08c35404b59ad
kushalchawla <kushalchawla18@gmail.com>
2016-03-05 15:35:57 +0530

    part1 main file updated

--- project2/part1/main.cpp
+++ project2/part1/main.cpp
@@ -1,1 +1,13 @@
 // main file
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	re_to_nfa();
+	nfa_to_dfa();
+	dfa_to_mindfa();
+	//final answer is in min_DFA 2d matrix...
+	cout<<"Conversion done successfully!"<<endl;
+	return 0;
+}
