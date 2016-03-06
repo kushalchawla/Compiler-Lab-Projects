dda515c5673185e106fabb12d1b32623b9715893
PrateekRaisinghani <raisinghani.kl.prateek@gmail.com>
2016-03-06 20:49:06 +0530

    DFA simulation now working

--- project2/part1/dfa_simulation.cpp
+++ project2/part1/dfa_simulation.cpp
@@ -6,7 +6,7 @@ bool dfa_simulate(string s)
 	int state=mindfa_start_state,i=0;
 	while(i<s.length())
 	{
-		state=min_DFA[state][s[i++]];
+		state=min_DFA[state][s[i++]-96];
 	}
 	if (mindfa_final_states.find(state)!=mindfa_final_states.end())
 		return 1;
